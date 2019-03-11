2a70e9d380dd3753f9e7b6e198f200cd0dd1da29
p_rykavishnikov <p.rykavishnikov@dev.crmm.ru>
2019-03-11 18:51:05 +0300

    Desided to move all chart window calculations out into Chart.js

--- src/app/Chart/Chart.js
+++ src/app/Chart/Chart.js
@@ -11,28 +11,28 @@ import './chart.scss';
 
 export default class Chart {
     constructor(data) {
+        // Cloning the data because of further mutation of it
         this.data = clone(data);
         this.element = document.createElement('div');
         this.element.classList.add(CHART_CLASS_NAME);
         this.element.innerHTML = html;
+
+        this.fraction = 1;
+        this.padding = 0;
     }
 
     init() {
         this.parseData();
-        this.chartWindow = new ChartWindow({
-            chartElement: this.element,
-            lines: this.lines,
-            xAxis: this.xAxis
-        });
-        this.chartWindow.render();
+        this.chartWindow = new ChartWindow(this.element);
     }
 
     parseData() {
         const { columns, names, colors } = this.data;
         [this.xAxis] = columns.splice(0, 1);
+        // Get rid of the "x" identificator
         this.xAxis.shift();
 
-        this.lines = columns.map(column => {
+        this.rawLines = columns.map(column => {
             const [lineName] = column.splice(0, 1);
             const name = names[lineName];
             const color = colors[lineName];
